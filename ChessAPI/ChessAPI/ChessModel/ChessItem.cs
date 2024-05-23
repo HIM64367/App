c23d5f567fabe48395d0ccf64ad3c198d3b9a800
Max Persent <esistenzapersesente@gmail.com>
2024-05-23 18:11:21 +0200

    #

--- ChessAPI/ChessAPI/ChessModel/ChessItem.cs
+++ ChessAPI/ChessAPI/ChessModel/ChessItem.cs
@@ -1,27 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualBasic;
 using System.Net.NetworkInformation;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json.Serialization;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace ChessAPI.ChessModel
 {
-    public class ChessPiece
-    {
-        [Key]
-        [JsonIgnore]
-        public int PieceId { get; set; }
-        public string? Id { get; set; }
-        public string? Name { get; set; }
-        public string? ImageUrl { get; set; }
-        public string? Position { get; set; }
-        public bool IsFirstMove { get; set; } = true;
-
-        
-    }
 
     public class ChessGame
     {
@@ -30,6 +15,22 @@ namespace ChessAPI.ChessModel
         public string? Turn { get; set; }
         public string GameState { get; set; } = "Normal";
         public List<ChessPiece>? Pieces { get; set; }
+    }
+
+    public class ChessPiece
+    {
+        [Key]
+        [JsonIgnore]
+        public int PieceId { get; set; }
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        [Required]
+        public string Position { get; set; }
+        public bool IsFirstMove { get; set; } = true;
+
     }
 
     public class ChessMove
